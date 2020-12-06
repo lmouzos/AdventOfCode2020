40bc6e39657136d6ae661ae714418bf9fcb5f5ba
Loukas Mouzos <lmouzos@bol.com>
2020-12-06 13:40:21 +0100

    clean up Day4

--- src/main/kotlin/day4/Day4.kt
+++ src/main/kotlin/day4/Day4.kt
@@ -4,10 +4,10 @@ import java.io.File
 
 class Day4 {
 
-    private val input = File("src/main/kotlin/day4/input.txt").readLines()
+    private val input = File("src/main/kotlin/day4/input.txt").readText().split("\n\n")
 
     fun puzzle1(): Int {
-        return getPassports().count {
+        return input.count {
             it.contains("byr")
                 .and(it.contains("iyr"))
                 .and(it.contains("eyr"))
@@ -19,70 +19,14 @@ class Day4 {
     }
 
     fun puzzle2(): Int {
-        return getPassports().filter {
-            it.contains("byr")
-                .and(it.contains("iyr"))
-                .and(it.contains("eyr"))
-                .and(it.contains("hgt"))
-                .and(it.contains("hcl"))
-                .and(it.contains("ecl"))
-                .and(it.contains("pid"))
-        }.map { it ->
-            it.split(' ')
-                .map { item ->
-                    val i = item.split(':')
-                    when (i[0]) {
-                        "byr" -> try {
-                            i[1].toInt() in 1920..2002
-                        } catch (e: NumberFormatException) {
-                            false
-                        }
-                        "iyr" -> try {
-                            i[1].toInt() in 2010..2020
-                        } catch (e: NumberFormatException) {
-                            false
-                        }
-                        "eyr" -> try {
-                            i[1].toInt() in 2020..2030
-                        } catch (e: NumberFormatException) {
-                            false
-                        }
-                        "hgt" -> "^(1[5-9][0-9]cm|[5-7][0-9]in)$".toRegex().matches(i[1])
-//                        {when {
-//                                i[1].endsWith("cm") -> {
-//                                    try {
-//                                        i[1].trimEnd('c', 'm').toInt() in 150..193
-//                                    } catch (e: NumberFormatException) {
-//                                        false
-//                                    }
-//                                }
-//                                i[1].endsWith("in") -> {
-//                                    try {
-//                                        i[1].trimEnd('i', 'n').toInt() in 59..76
-//                                    } catch (e: NumberFormatException) {
-//                                        false
-//                                    }
-//                                }
-//                                else -> false
-//                            }}
-                        "hcl" -> "^#([a-f0-9]{6})$".toRegex().matches(i[1]) // hex color regex
-                        "ecl" -> "^(amb|blu|brn|gry|grn|hzl|oth)$".toRegex().matches(i[1])
-                        "pid" -> "^[0-9]{9}$".toRegex().matches(i[1]) //i[1].length == 9 && i[1].filter { it.isDigit() }.count() == 9 // all 9 chars are digits
-                        else -> false
-                    }
-                }.count { it }
-        }.count { it == 7 }
-    }
-
-    private fun getPassports(): List<String> {
-        val passports = mutableMapOf<Int, String>()
-        var ir = 0
-        input.forEach { s ->
-            if (s.isEmpty()) {
-                ir++
-            }
-            passports[ir] = passports[ir]?.plus(" $s") ?: s
+        return input.count {
+            it.contains("byr:(19[2-9][0-9]|200[0-2])".toRegex())
+                .and(it.contains("iyr:(201[0-9]|2020)".toRegex()))
+                .and(it.contains("eyr:(202[0-9]|2030)".toRegex()))
+                .and(it.contains("hgt:(1[5-8][0-9]cm|19[0-3]cm|59in|6[0-9]in|7[0-6]in)".toRegex()))
+                .and(it.contains("hcl:#[a-f0-9]{6}".toRegex()))
+                .and(it.contains("ecl:(amb|blu|brn|gry|grn|hzl|oth)".toRegex()))
+                .and(it.contains("pid:[0-9]{9}(\\D|$)".toRegex()))
         }
-        return passports.values.toList()
     }
 }
