620ddb80af9ca3a07545f50c5dd19a7a3260f306
Loukas Mouzos <lmouzos@bol.com>
2020-12-06 13:43:17 +0100

    clean up main

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,16 +1,21 @@
+import day1.Day1
+import day2.Day2
+import day3.Day3
+import day4.Day4
+import day5.Day5
 import day6.Day6
 
-fun main(args: Array<String>) {
-//    println(Day1().puzzle1())
-//    println(Day1().puzzle2())
-//    println(Day2().puzzle1())
-//    println(Day2().puzzle2())
-//    println(Day3().puzzle1())
-//    println(Day3().puzzle2())
-//    println(Day4().puzzle1())
-//    println(Day4().puzzle2())
-//    println(Day5().puzzle1())
-//    println(Day5().puzzle2())
+fun main() {
+    println(Day1().puzzle1())
+    println(Day1().puzzle2())
+    println(Day2().puzzle1())
+    println(Day2().puzzle2())
+    println(Day3().puzzle1())
+    println(Day3().puzzle2())
+    println(Day4().puzzle1())
+    println(Day4().puzzle2())
+    println(Day5().puzzle1())
+    println(Day5().puzzle2())
     println(Day6().puzzle1())
     println(Day6().puzzle2())
 }
